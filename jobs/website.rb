df332843afb3a10f7986779359f640cb5a080f2d
Johannes Engelke <johannes.engelke@sap.com>
2019-08-01 09:42:42 +0200

    Added google information

--- jobs/website.rb
+++ jobs/website.rb
@@ -7,26 +7,32 @@ require 'google/apis/analyticsreporting_v4'
 
 
 SCHEDULER.every '60m', :first_in => 0 do |job|  
-  current_users = user_count(Google::Apis::AnalyticsreportingV4::DateRange.new(start_date: '30DaysAgo', end_date: 'yesterday'))
-  last_users = user_count(Google::Apis::AnalyticsreportingV4::DateRange.new(start_date: '60DaysAgo', end_date: '30DaysAgo'))
-
-  puts current_users
-  puts last_users
-
-end
-
-def user_count(date_range)
   scopes =  ['https://www.googleapis.com/auth/analytics.readonly',
     'https://www.googleapis.com/auth/analytics']
-  
+
   auth = Google::Auth::ServiceAccountCredentials.make_creds(
     json_key_io: File.open(ENV["GOOGLE_API_KEY"]),
     scope: scopes)
   analytics = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
   analytics.authorization = auth
 
-  metric = Google::Apis::AnalyticsreportingV4::Metric.new(expression: 'ga:users')
+  current = Google::Apis::AnalyticsreportingV4::DateRange.new(start_date: '30DaysAgo', end_date: 'yesterday')
+  last = Google::Apis::AnalyticsreportingV4::DateRange.new(start_date: '60DaysAgo', end_date: '30DaysAgo')
   
+  current_users = user_count(analytics, current)
+  last_users = user_count(analytics, last)
+
+  current_duration = user_duration(analytics, current)
+  last_duration = user_duration(analytics, last)
+
+  puts current_users
+  puts last_users
+  puts current_duration
+  puts last_duration
+end
+
+def user_count(analytics, date_range)
+  metric = Google::Apis::AnalyticsreportingV4::Metric.new(expression: 'ga:users')
   dimension = Google::Apis::AnalyticsreportingV4::Dimension.new(name: 'ga:userType')
 
   request = Google::Apis::AnalyticsreportingV4::GetReportsRequest.new(
@@ -53,5 +59,21 @@ def user_count(date_range)
   end
 
   return [new_visitors, returning_visitors]
+end
+
+def user_duration(analytics, date_range)
+  metric = Google::Apis::AnalyticsreportingV4::Metric.new(expression: 'ga:avgSessionDuration')
+
+  request = Google::Apis::AnalyticsreportingV4::GetReportsRequest.new(
+    report_requests: [Google::Apis::AnalyticsreportingV4::ReportRequest.new(
+      view_id: ENV['PAGE_ID'],
+      metrics: [metric],
+      date_ranges: [date_range]
+    )]
+  ) 
+
+  response = analytics.batch_get_reports(request)
+
+  return (response.reports[0].data.rows[0].metrics[0].values[0].to_f / 60).round(2)
 end
 
